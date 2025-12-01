23b2ea5b784bf2dd56bbeb76fc2d5fd75c8302c5
Dogu_ <dogu_rog@hotmail.com>
2025-12-01 22:15:37 +0300

    drug entity

---
+++ DataAccess/Concrete/EntityFramework/PharmacyContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class PharmacyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer();
+        }
+
+        public DbSet<Drug> Drugs { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
---
+++ Entities/Concrete/Drug.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Drug : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Indications { get; set; }
+        public string Contraindications { get; set; }
+        public DateTime? ExprirationDate { get; set; }
+    }
+}
